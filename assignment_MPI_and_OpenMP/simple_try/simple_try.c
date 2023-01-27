4265118eacb3a63f0304754fa1807a478b641657
wolneberg <ingrid_marie@wolneberg.no>
2023-01-27 15:52:27 +0100

    board variable

--- assignment_MPI_and_OpenMP/simple_try/simple_try.c
+++ assignment_MPI_and_OpenMP/simple_try/simple_try.c
@@ -6,6 +6,9 @@
 // change row and column value to set the canvas size
 const int ROW = 900;
 const int COL = 900;
+
+int board [ROW][COL];
+
 // const int PATTERN_ROW=GLIDER_HEIGHT;
 // const int PATTERN_COL=GLIDER_WIDTH;
 
@@ -60,45 +63,40 @@ void init_canvas(int a[][COL]){
         }
     }
 }
-void calculate_next_generation(int a[][COL], int b[][COL]) {
-    init_canvas(b);
+void calculate_next_generation(int a[][COL]) {
     int neighbour_live_cell;
+    int new_b[ROW][COL];
     for (int i = 0; i < ROW; i++) {
         for (int j = 0; j < COL; j++) {
             neighbour_live_cell = count_live_neighbour_cell(a, i, j);
             if (a[i][j] == 1 && (neighbour_live_cell == 2 || neighbour_live_cell == 3)) {
                 if (is_border(i, j) == -1) {
-                    b[i][j] = 1;
+                    new_b[i][j] = 1;
                 }
             } else if (a[i][j] == 0 && neighbour_live_cell == 3) {
                 if (is_border(i, j) == -1) {
-                    b[i][j] = 1;
+                    new_b[i][j] = 1;
                 }
             } else {
-                b[i][j] = 0;
+                new_b[i][j] = 0;
             }
         }
     }
-}
-
-void calculate_n_generation(int a[][COL], int b[][COL],int time){
-    // int res[ROW][COL];
-    // init_canvas(res);
-    for (int i = 0; i < time; i++) {
-        if (i % 2 == 0) {
-            calculate_next_generation(a, b);
-            // init_canvas(a);
-            // print_2d_array(b);
-        } else {
-            calculate_next_generation(b, a);
-            // init_canvas(b);
-            // print_2d_array(a);
+    for (int i = 0; i < ROW; i++){
+        for (int j = 0; j < COL; j++){
+            board[i][j] = new_b[i][j];
         }
     }
 }
 
+void calculate_n_generation(int a[][COL],int time){
+    for (int i = 0; i < time; i ++){
+        calculate_next_generation(board);
+    }
+}
 
-void insert_pattern(uint8_t pattern[][PATTERN_COL], int canvas[][COL], int pattern_row, int pattern_col, int canvas_row, int canvas_col, int start_row, int start_col) {
+
+void insert_pattern(uint8_t pattern[][PATTERN_COL], int canvas[][COL], int pattern_row, int pattern_col, int start_row, int start_col) {
     for (int i = 0; i < pattern_row; i++) {
         for (int j = 0; j < pattern_col; j++) {
             canvas[i + start_row][j + start_col] = pattern[i][j];
@@ -116,46 +114,36 @@ void test_count_live_cell(int a[][COL],int expected_output) {
             printf("Test failed: count_live_cell(a) returned %d, expected %d\n", output, expected_output);
         }
 }
-// void glider_test(int generation,int expected){
-//     const int PATTERN_ROW=GLIDER_HEIGHT;
-//     const int PATTERN_COL=GLIDER_WIDTH;
+/*
+void glider_test(int generation,int expected){
+     init_canvas(board);
+     insert_pattern(glider,board,PATTERN_ROW,PATTERN_COL,1,1);
 
-//     int a[ROW][COL], b[ROW][COL];
-//     init_canvas(a);
-//     insert_pattern(glider,a,PATTERN_ROW,PATTERN_COL,ROW,COL,1,1);
+     calculate_n_generation(board,generation);
+     test_count_live_cell(board,expected);
+ }
+*/
 
-//     calculate_n_generation(a,b,generation);
-//     test_count_live_cell(b,expected);
-// }
+/*
+void beehive_test(int generation,int expected){
+    init_canvas(board);
+    insert_pattern(beehive,board,PATTERN_ROW,PATTERN_COL,1,1);
 
-
-// void beehive_test(int generation,int expected){
-    
-//     int a[ROW][COL], b[ROW][COL];
-//     init_canvas(a);
-//     insert_pattern(beehive,a,PATTERN_ROW,PATTERN_COL,ROW,COL,1,1);
-
-//     calculate_n_generation(a,b,generation);
-//     test_count_live_cell(b,expected);
-// }
+    calculate_n_generation(board,generation);
+    test_count_live_cell(board,expected);
+}
+*/
 
 void grower_test(int generation,int expected){
     
-    int a[ROW][COL], b[ROW][COL];
-    init_canvas(a);
-    init_canvas(b);
-    insert_pattern(grower,a,PATTERN_ROW,PATTERN_COL,ROW,COL,300,300);
+    init_canvas(board);
+    insert_pattern(grower,board,PATTERN_ROW,PATTERN_COL,100,100);
 
-    calculate_n_generation(a,b,generation);
-    if(generation%2==0){
-        test_count_live_cell(a,expected);
-    }else{
-        test_count_live_cell(b,expected);
+    calculate_n_generation(board,generation);
+    test_count_live_cell(board, expected);
     }
-}
 
-int main()
-{   
+int main(){   
     // glider_test(50,5);
     // beehive_test(10,6);
     
