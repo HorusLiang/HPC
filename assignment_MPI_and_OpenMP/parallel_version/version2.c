6bd2c0b0fa895ffe6ecb1b46277f2cb4f30188f2
wolneberg <ingrid_marie@wolneberg.no>
2023-01-27 16:46:57 +0100

    parallel version with board

---
+++ assignment_MPI_and_OpenMP/parallel_version/version2.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <omp.h>
+#include <mpi.h>
+#include <math.h>
+#include "./headers/beehive.h"
+#include "./headers/glider.h"
+#include "./headers/grower.h"
+ 
+// change row and column value to set the canvas size
+static const int ROW = 3000;
+static const int COL = 3000;
+
+// const int PATTERN_ROW=GLIDER_HEIGHT;
+// const int PATTERN_COL=GLIDER_WIDTH;
+
+// const int PATTERN_ROW=BEEHIVE_HEIGHT;
+// const int PATTERN_COL=BEEHIVE_WIDTH;
+
+const int PATTERN_ROW=GROWER_HEIGHT;
+const int PATTERN_COL=GROWER_WIDTH;
+
+// Initialize MPI
+int rank, num_procs;
+
+
+// Divide loop iterations among processes
+int rows_per_proc;
+int start_row ;
+int end_row;
+
+int is_border(int i,int j){
+    if (i == 0 || i == ROW-1 || j == 0 || j == COL-1){
+        return 1;
+    }
+    return -1;
+}
+ 
+// returns the count of alive neighbours
+int count_live_neighbour_cell(int a[ROW][COL], int r, int c)
+{
+    int i, j, count = 0;
+    for (i = r - 1; i <= r + 1; i++) {
+        for (j = c - 1; j <= c + 1; j++) {
+            if ((i == r && j == c) || (i < 0 || j < 0)
+                || (i >= ROW || j >= COL)) {
+                continue;
+            }
+            if (a[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+int count_live_cell(int a[][COL]) {
+    int count = 0;
+    #pragma omp parallel for
+    for  (int i = start_row; i < end_row; i++)  {
+        for (int j = 0; j < COL; j++) {
+            if (a[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+    // Gather all the results from each process to the root process
+    MPI_Gather(a[start_row], rows_per_proc*COL, MPI_INT, a, rows_per_proc*COL, MPI_INT, 0, MPI_COMM_WORLD);
+    return count;
+}
+
+void init_canvas(int a[][COL]){
+    int i, j;
+
+    #pragma omp parallel for
+    for (i = start_row; i < end_row; i++) {
+        for (j = 0; j < COL; j++) {
+            a[i][j]=0;  
+        }
+    }
+    // Gather all the results from each process to the root process
+    MPI_Gather(a[start_row], rows_per_proc*COL, MPI_INT, a, rows_per_proc*COL, MPI_INT, 0, MPI_COMM_WORLD);
+}
+void calculate_next_generation(int a[][COL]) {
+    int neighbour_live_cell;
+    int new_b[ROW][COL];
+    
+    #pragma omp parallel for private(neighbour_live_cell)
+    for (int i = start_row; i < end_row; i++) {
+        for (int j = 0; j < COL; j++) {
+            neighbour_live_cell = count_live_neighbour_cell(a, i, j);
+            if (a[i][j] == 1 && (neighbour_live_cell == 2 || neighbour_live_cell == 3)) {
+                if (is_border(i, j) == -1) {
+                    new_b[i][j] = 1;
+                }
+            } else if (a[i][j] == 0 && neighbour_live_cell == 3) {
+                if (is_border(i, j) == -1) {
+                    new_b[i][j] = 1;
+                }
+            } else {
+                new_b[i][j] = 0;
+            }
+        }
+    }
+    // Gather all the results from each process to the root process
+    MPI_Gather(new_b[start_row], rows_per_proc*COL, MPI_INT, new_b, rows_per_proc*COL, MPI_INT, 0, MPI_COMM_WORLD);
+    for (int i = 0; i < ROW; i++){
+        for (int j = 0; j < COL; j++){
+            a[i][j] = new_b[i][j];
+        }
+    }
+}
+
+void calculate_n_generation(int a[][COL], int time){
+    for (int i = 0; i < time; i ++){
+        calculate_next_generation(a);
+    }
+}
+
+
+void insert_pattern(uint8_t pattern[][PATTERN_COL], int canvas[][COL], int pattern_row, int pattern_col, int start_row, int start_col) {
+    for (int i = 0; i < pattern_row; i++) {
+        for (int j = 0; j < pattern_col; j++) {
+            canvas[i + start_row][j + start_col] = pattern[i][j];
+        }
+    }
+}
+
+
+
+void test_count_live_cell(int a[][COL],int expected_output) {
+        int output = count_live_cell(a);
+        int final_output;
+        MPI_Reduce(&output, &final_output, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+        if(rank==0){
+            if (final_output == expected_output) {
+            printf("Test passed: count_live_cell(a) returned %d\n", final_output);
+            } else {
+                printf("Test failed: count_live_cell(a) returned %d, expected %d\n", final_output, expected_output);
+            }
+        }
+}
+/*
+void glider_test(int generation,int expected){
+     int board [ROW][COL];
+     init_canvas(board);
+     insert_pattern(glider,board,PATTERN_ROW,PATTERN_COL,1,1);
+
+     calculate_n_generation(board,generation);
+     test_count_live_cell(board,expected);
+ }
+*/
+/*
+void beehive_test(int generation,int expected){
+    int board [ROW][COL];
+    init_canvas(board);
+    insert_pattern(beehive,board,PATTERN_ROW,PATTERN_COL,1,1);
+
+    calculate_n_generation(board,generation);
+    test_count_live_cell(board,expected);
+}
+*/
+
+void grower_test(int generation, int expected){
+    int board [ROW][COL];
+    init_canvas(board);
+    insert_pattern(grower,board,PATTERN_ROW,PATTERN_COL,100,100);
+
+    calculate_n_generation(board,generation);
+    test_count_live_cell(board, expected);
+}
+
+int main()
+{   
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    // printf("num_procs:%d \n",num_procs);
+
+    rows_per_proc = (int)ceil((double)ROW / num_procs);
+    start_row = rank * rows_per_proc;
+    end_row = start_row + rows_per_proc;
+    if(end_row > ROW) end_row = ROW;
+    
+    
+    double start,end;
+    
+    start=omp_get_wtime();
+    // glider_test(50,5);
+    // beehive_test(10,6);
+    
+    // grower_test(100,138);
+    // grower_test(0,20);
+    // grower_test(1,23);
+    // grower_test(2,24);
+    // grower_test(3,28);
+    // grower_test(4,31);
+    // grower_test(5,35);
+    // grower_test(6,35);
+    // grower_test(7,35);
+    // grower_test(8,43);
+    // grower_test(9,41);
+    grower_test(10,49);
+    // grower_test(5000,3647);
+    end=omp_get_wtime();
+    // Print result
+    // printf("Obtained in %f seconds, rank=%d\n",end - start,rank);
+    if(rank==0){
+        printf("Obtained in %f seconds\n",end - start);
+    }
+    
+    
+    
+
+    // Finalize MPI
+    MPI_Finalize();
+    return 0;
+}
